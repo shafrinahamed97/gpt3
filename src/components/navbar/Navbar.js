1ca7d8e8b525dfcf39f054a0554551ce865d6980
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-04-02 17:52:49 +0600

    develpe nav section & design button continue

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './Navbar.css';
+import logo from '../../assests/logo.svg';
+
+
 
 const Navbar = () => {
     return (
@@ -9,6 +12,17 @@ const Navbar = () => {
                 <div className="gpt3-navbar-links-logo">
                     <img src={logo} alt="logo" srcset="" />
                 </div>
+                <div className="gpt3-navbar-links-container">
+                    <p><a href="#home">Home</a></p>
+                    <p><a href="#wgpt3">What is GPT3?</a></p>
+                    <p><a href="#possibility">OPen AI</a></p>
+                    <p><a href="#features">Case Studies</a></p>
+                    <p><a href="#blog">Library</a></p>
+                </div>
+            </div>
+            <div className="gpt3-navbar-sign">
+                <p>Sign in</p>
+                <button type='button'>Sign Up</button>
             </div>
         </div>
     );
