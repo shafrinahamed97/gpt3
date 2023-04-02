473018028f99e3e708ef47a05679cd7bc3d3b5f1
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-04-02 23:26:28 +0600

    finished navbar section & start Header section

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -3,8 +3,11 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is header section</h2>
+        <div className='gpt3-header section-padding' id='home'>
+            <div className='gpt3-header-content'>
+                <h1 className='gradient-text'>Let's Build Something amazing with GPT-3 Open AI</h1>
+            </div>
+
         </div>
     );
 };
