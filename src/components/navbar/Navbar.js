94297598ddb5466d05532dd2ca16c6170ef794d0
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-04-02 16:49:22 +0600

    pictures collection from internet

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>This is Navbar setcion</h2>
+        <div className="gpt3-navbar">
+            <div className="gpt3-navbar-links">
+                <div className="gpt3-navbar-links-logo">
+                    <img src={logo} alt="logo" srcset="" />
+                </div>
+            </div>
         </div>
     );
 };
