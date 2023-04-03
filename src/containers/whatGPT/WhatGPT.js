f7c9b6f76cb268b0446ae99eeefff1537fb78859
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-04-03 14:09:57 +0600

    added whatgpt3 section and basic setup in JS file

--- src/containers/whatGPT/WhatGPT.js
+++ src/containers/whatGPT/WhatGPT.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Feature from '../../components/feature/Feature';
 import './WhatGPT.css';
 
 const WhatGPT = () => {
     return (
-        <div>
-            <h2>This is GPT section</h2>
+        <div className='gpt3-whatgpt3 section-margin' id='whpt3'>
+            <div className="gpt3-whatgpt3-feature">
+                <Feature>
+
+                </Feature>
+            </div>
+            <div className="gpt3-whatgpt3-header">
+                <h1 className='gradient-text'>The posibilities are beyond your imagination</h1>
+                <p>Explore The Library</p>
+            </div>
+            <div className="gpt3-whatgpt3-container">
+                <Feature></Feature>
+                <Feature></Feature>
+                <Feature></Feature>
+            </div>
         </div>
     );
 };
