8cd2ba9a7c8efbea66994fe0cf9af637a0e8b736
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-04-03 23:11:56 +0600

    finished CTA section, design this section & responsive this section

--- src/components/cta/CTA.js
+++ src/components/cta/CTA.js
@@ -3,8 +3,14 @@ import './CTA.css';
 
 const CTA = () => {
     return (
-        <div>
-            <h2>This is CTA section</h2>
+        <div className='gpt3-cta'>
+            <div className="gpt3-cta-content">
+                <p>Request Early Access to Get Started </p>
+                <h3>Register today & Start exploring the endless possibilities</h3>
+            </div>
+            <div className="gpt3-cta-btn">
+                <button type='button'>Get Started</button>
+            </div>
         </div>
     );
 };
