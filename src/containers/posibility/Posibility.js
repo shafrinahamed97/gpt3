c71040de4ff6ccb014da66206690bed51150e88d
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-04-03 22:04:55 +0600

    finished possiblity section, design this section & responsive this section

--- src/containers/posibility/Posibility.js
+++ src/containers/posibility/Posibility.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Posibility.css';
+import possibility from '../../assests/possibility.png';
 
 const Posibility = () => {
     return (
-        <div>
-            <h2>This is Posibility section</h2>
+        <div className='gpt3-possibility section-padding' id='posibility'>
+            <div className="gpt3-posibility-image">
+                <img src={possibility} alt="possibility" />
+            </div>
+            <div className="gpt3-possibility-content">
+                <h4>Request Early Access to Get Started </h4>
+                <h1 className='gradient-text'>The possibilities are beyond your imagination </h1>
+                <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                <h4>Request Early Access to Get Started</h4>
+            </div>
         </div>
     );
 };
--- src/containers/whatGPT/WhatGPT.js
+++ src/containers/whatGPT/WhatGPT.js
@@ -4,7 +4,7 @@ import Feature from '../../components/feature/Feature';
 
 const WhatGPT = () => {
     return (
-        <div className='gpt3-whatgpt3 section-margin id-wgpt3'>
+        <div className='gpt3-whatgpt3 section-margin ' id='wgpt3'>
 
             <div className="gpt3-whatgpt3-feature">
                 <Feature title="What is 3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."></Feature>
