41b6c6842d4a65d84b46ab5920b7f83c9f838d3e
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-04-03 16:05:17 +0600

    added brands section & style this seciton

--- src/components/brands/Brands.js
+++ src/components/brands/Brands.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 import './Brands.css';
 const Brands = () => {
     return (
-        <div>
-            <h2>This is brads section</h2>
+        <div className='gpt3-brand section-padding'>
+            <div>
+                <img src={google} alt="" />
+            </div>
+            <div>
+                <img src={slack} alt="" />
+            </div>
+            <div>
+                <img src={atlassian} alt="" srcset="" />
+            </div>
+            <div>
+                <img src={dropbox} alt="" />
+            </div>
+            <div>
+                <img src={shopify} alt="" />
+            </div>
+
         </div>
     );
 };
--- src/containers/whatGPT/WhatGPT.js
+++ src/containers/whatGPT/WhatGPT.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import Feature from '../../components/feature/Feature';
 import './WhatGPT.css';
 
 const WhatGPT = () => {
     return (
-        <div className='gpt3-whatgpt3 section-margin' id='whpt3'>
-            <div className="gpt3-whatgpt3-feature">
-                <Feature>
+        <div>
 
-                </Feature>
-            </div>
-            <div className="gpt3-whatgpt3-header">
-                <h1 className='gradient-text'>The posibilities are beyond your imagination</h1>
-                <p>Explore The Library</p>
-            </div>
-            <div className="gpt3-whatgpt3-container">
-                <Feature></Feature>
-                <Feature></Feature>
-                <Feature></Feature>
-            </div>
         </div>
     );
 };
